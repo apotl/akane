2776515596e3f5afb817acd9a5bdd179d839d440
Alec Potluri <brookdalealec@gmail.com>
2016-09-04 17:51:37 -0500

    Added add cmd to ctl

--- akane.py
+++ akane.py
@@ -28,6 +28,17 @@ class Akane():
 		for board_scraper in self.board_scrapers:
 			board_scraper.initialize_available_thread_scrapers()
 			board_scraper.start()
+
+	def list_boards(self):
+		return list(self.session.query(models.Board).all())
+
+	def add_board(self,board):
+		matches = self.session.query(models.Board).filter(models.Board.board_name == board.board_name)
+		if len(list(matches)) > 0:
+			return False
+		self.session.add(board)
+		self.session.commit()
+		return True
 	
 class Ctl():
 	def __init__(self):
@@ -60,3 +71,73 @@ class Ctl():
 		"""Quits the Akane ctl."""
 		print('Quitting the Akane ctl.')
 		self._loopctl = False
+
+	def list(self):
+		"""Prints a list of boards being archived with set archive options."""
+		boards = []
+		for board in self._akane.list_boards():
+			boards += [{'board_name': board.board_name,
+				'frequency': board.frequency,
+				'quiet': board.quiet,
+				'get_images': board.get_images}]
+
+		print('List of boards Akane will archive:')
+		print('\t' + 'Board Name' +
+			'\t' + 'Frequency' + 
+			'\t' + 'Log requests?' +
+			'\t' + 'Images?')
+		for board in boards:
+			print('\t' + board['board_name'] +
+				'\t\t' + str(board['frequency']) + 
+				'\t\t' + str(board['quiet']) +
+				'\t\t' + str(board['get_images']))
+
+	def add(self):
+		"""Adds a board to Akane's database for archiving."""
+		board = models.Board()
+		while not board.board_name:
+			print('What board should Akane archive? Enter the board name, excluding slashes: ', end = '')
+			board.board_name = input()
+		while not board.frequency or board.frequency < 1:
+			print('How frequent should the specified board be archived, in minutes? ', end = '')
+			try:
+				opt_freq = int(input())
+				board.frequency = opt_freq
+			except:
+				pass
+		while 1:
+			print('Should all requests made while archiving this board be logged? (y/N) ', end = '')
+			quiet_opt = input()
+			if quiet_opt.lower() == 'y':
+				board.quiet = 1
+				break
+			elif quiet_opt.lower() == 'n' or quiet_opt == '':
+				board.quiet = 0
+				break
+		while 1:
+			print('Should images, if present on posts, be downloaded? (Y/n) ', end = '')
+			image_opt = input()
+			if image_opt.lower() == 'n':
+				board.get_images = 0
+				break
+			elif image_opt.lower() == 'y' or image_opt == '':
+				board.get_images = 1
+				break
+
+		print('')
+		if not self._akane.add_board(board):
+			print('ERROR: Supplied board /'+board.board_name+'/ already exists in the database')
+		else:
+			print('Board /'+board.board_name+'/ added for archiving.')
+
+	def enable(self):
+		"""(Not implemented) Enable archiving on a selected board."""
+		pass
+
+	def disable(self):
+		"""(Not implemented) Disable archiving a selected board."""
+		pass
+
+	def edit(self):
+		"""(Not implemented) Edit the archive options of a board."""
+		pass
